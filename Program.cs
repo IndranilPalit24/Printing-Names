1f753e73dde699434d1c453f6e90d6c3b6d93b04
Indranil_Palit <73847100+IndranilPalit24@users.noreply.github.com>
2021-09-01 19:17:45 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace printing_names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People P1 = new People("John","Krasinski",40);
+            Console.WriteLine(P1.find_details());
+        }
+    }
+}
